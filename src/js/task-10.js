0a8b8356ea4671aac3f648cf1e47c86288633d10
Nataliya Kim <magistr1982@gmail.com>
2023-02-26 14:40:21 +0200

    Update task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -8,6 +8,27 @@ const createButtonEl = document.querySelector("#controls [data-create]");
 const destroyButtonEl = document.querySelector("#controls [data-destroy]");
 const boxesContainerEl = document.querySelector("#boxes");
 
-// не вистачило мені сили і терпіння на цю задачу.. )))
-// але зроблю її для себе 100%
-// 
+function createBoxes(amount) {
+  let boxSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const boxEl = document.createElement("div");
+    boxEl.classList.add("box__item");
+    boxEl.style.backgroundColor = getRandomHexColor();
+    boxEl.style.width = `${boxSize}px`;
+    boxEl.style.height = `${boxSize}px`;
+    boxesContainerEl.appendChild(boxEl);
+    boxSize += 10;
+  }
+};
+
+const handleCreateButtonClick = () => {
+  const amount = inputEl.value;
+  createBoxes(amount);
+};
+
+const handleDestroyButtonClick = () => {
+  boxesContainerEl.innerHTML = '';
+};
+
+createButtonEl.addEventListener("click", handleCreateButtonClick);
+destroyButtonEl.addEventListener("click", handleDestroyButtonClick);
